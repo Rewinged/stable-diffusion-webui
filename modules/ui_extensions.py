39af2c43278401cffd80a7455c1b00c519549f70
Owlcoholic <rewinged28@gmail.com>
2023-05-11 01:37:09 +0930

    Revert "Revert "Revert "Merge branch 'master' into img2img-enhance"""
    This reverts commit 34d2f51e00554f9185422742c1acfca01c2a5ea4.

--- modules/ui_extensions.py
+++ modules/ui_extensions.py
@@ -63,9 +63,6 @@ def check_updates(id_task, disable_list):
 
         try:
             ext.check_updates()
-        except FileNotFoundError as e:
-            if 'FETCH_HEAD' not in str(e):
-                raise
         except Exception:
             print(f"Error checking updates for {ext.name}:", file=sys.stderr)
             print(traceback.format_exc(), file=sys.stderr)
@@ -90,8 +87,6 @@ def extension_table():
     """
 
     for ext in extensions.extensions:
-        ext.read_info_from_repo()
-
         remote = f"""<a href="{html.escape(ext.remote or '')}" target="_blank">{html.escape("built-in" if ext.is_builtin else ext.remote or '')}</a>"""
 
         if ext.can_update:
